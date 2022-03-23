35cb67b397218bc1dd811f984025b124c2b758d8
murilo <murilo.varges@gmail.com>
2022-03-23 11:00:46 -0300

    exemplos

---
+++ aula5/ex2.php
@@ -0,0 +1,9 @@
+<?php
+    $n1 = 5;
+    $n2 = 3;
+    $media = ($n1 + $n2)/2;
+    if($media >= 7)
+        echo "Você foi aprovado";
+    else
+        echo "Você foi reprovado";
+?>
---
+++ aula5/ex3.php
@@ -0,0 +1,9 @@
+<?php
+    $op = 'a';
+    switch($op){
+        case 'a': echo "Abrir"; break;
+        case 'f': echo "Fechar"; break;
+        case 's': echo "Salvar"; break;
+        default: echo "Sair"; break;
+    }
+?>
