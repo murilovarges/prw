25429a081fe0faea5c971f85fb16be4f9c56759c
Murilo <murilo.varges@gmail.com>
2022-05-11 11:42:49 -0300

    alterar

--- projeto01/listar_usuarios.php
+++ projeto01/listar_usuarios.php
@@ -8,7 +8,6 @@
 
     //retorna a primeira linha
     //$row = mysqli_fetch_array($result);
-
 ?>
 
 <!DOCTYPE html>
@@ -41,7 +40,7 @@
                 echo "<td><a href='altera_usuario.php?id_usuario=".$row['id_usuario']."'>" .$row['nome_usuario']. "</a></td>";
                 echo "<td>" .$row['email_usuario']. "</td>";
                 echo "<td>" .$row['telefone_usuario']. "</td>";
-                echo "<td><a href='excluir_usuario.php?id_usuario=".$row['id_usuario']."'>Excluir</a></td>";                
+                echo "<td><a href='excluir_usuario.php?id_usuario=".$row['id_usuario']."'>Excluir</a></td>";
                 echo "</tr>";
             }
         ?>
