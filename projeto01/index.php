34df58f411b9fa3b5ed90f4c37808351cea3f1d6
Murilo <murilo.varges@gmail.com>
2022-05-11 09:01:18 -0300

    teste git.

--- projeto01/index.php
+++ projeto01/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu principal</title>
+    <title>Menu principal - IFSP</title>
     <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
